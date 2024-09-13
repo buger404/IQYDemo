0e78f15459e50b980e65c402150b8a62438c6070
Buger404 <ris_vb@126.com>
2024-09-13 16:42:53 +0800

    chores: initial commit

---
+++ entry/src/main/ets/pages/Index.js
@@ -0,0 +1,15 @@
+"use strict";
+const menuList = ['直播', '首页', '热点', '电视剧', '电影', '娱乐', '小说'];
+function MenuButton() {
+    
+  
+        .fontColor(Color.White);
+}
+struct Index extends   {
+    constructor() { }
+    build() {
+            .backgroundColor('#111214')
+            .height('100%');
+    }
+}
+//# sourceMappingURL=Index.js.map
